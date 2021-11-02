964421edac880c717c0f7b4c91d470fc75524534
TadeasHofman <czgandy@gmail.com>
2021-11-02 07:43:30 +0100

    test

---
+++ another.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<style>
+    h1 { color: black; font-family: "Comic Sans MS";padding-left: 50px;padding-right: 50px; }
+    p {color: black;font-family: "Comic Sans MS";padding-left: 50px;padding-right: 50px; }
+
+</style>
+</head>
+<body>
+<h1>Praxe v marbesu 2021</h1>
+    <span style = "color:#ff0000;padding-left: 50px"><?php echo $_GET["text"]; ?></span>
+<p>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam faucibus mi quis velit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Donec quis nibh at felis congue commodo. Integer pellentesque quam vel velit. Nullam sit amet magna in magna gravida vehicula. Nullam eget nisl. Suspendisse nisl. Integer pellentesque quam vel velit. Duis viverra diam non justo. Integer imperdiet lectus quis justo. Duis pulvinar. Nullam dapibus fermentum ipsum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Mauris tincidunt sem sed arcu.
+</p>
+<p>
+    Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Aenean fermentum risus id tortor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam dictum tincidunt diam. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Pellentesque arcu. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Praesent id justo in neque elementum ultrices. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat. Etiam commodo dui eget wisi. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+</p>
+<form action="another.php" method="get">
+    text <input type="text" name="text"><br>
+
+    <input type="submit">
+</form>
+</body>
+</html>
+
